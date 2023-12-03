2bd84dcd5ab189e5b0ca0c1a0e953d99d73addf7
DavidGuambana <112259062+DavidGuambana@users.noreply.github.com>
2023-12-02 22:15:30 -0500

    a

--- src/main/java/com/clinica/services/IHistoria_ClinicaService.java
+++ src/main/java/com/clinica/services/IHistoria_ClinicaService.java
@@ -5,7 +5,7 @@ import com.clinica.entity.Historia_Clinica;
 
 public interface IHistoria_ClinicaService {
 	public List<Historia_Clinica> findAll();
-	public Historia_Clinica findById(Long id);
+	public Historia_Clinica findById(String id);
 	public Historia_Clinica save(Historia_Clinica historia_clinica);
-	public void delete (Long id);
+	public void delete (String id);
 }
--- src/main/java/com/clinica/services/IRegistro_Parte_DiarioService.java
+++ src/main/java/com/clinica/services/IRegistro_Parte_DiarioService.java
@@ -1,5 +1,12 @@
 package com.clinica.services;
 
-public interface IRegistro_Parte_DiarioService {
+import java.util.List;
 
+import com.clinica.entity.Registro_Parte_Diario;
+
+public interface IRegistro_Parte_DiarioService {
+	public List<Registro_Parte_Diario> findAll();
+	public Registro_Parte_Diario finfById(Long id);
+	//
+	;
 }
